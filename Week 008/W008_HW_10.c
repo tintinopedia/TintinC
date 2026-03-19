2b9d3ee9d75760ddbe2a7ea837f75ab635441a64
Ahaan <ahaan.basumojumdar@gmail.com>
2026-03-19 15:45:35 +0530

    Sample Loop

--- Week 008/W008_HW_10.c
+++ Week 008/W008_HW_10.c
@@ -14,3 +14,21 @@ Think:
 spaces = rows - i
 stars = i
 */
+
+
+#include <stdio.h>
+
+int main() {
+    // Outer loop for Rows
+    for (int i = 1; i <= 3; i++) {
+
+        // Inner loop for Columns
+        for (int j = 1; j <= 3; j++) {
+            printf("* ");
+        }
+
+        // Move to a new line after each row is finished
+        printf("\n");
+    }
+    return 0;
+}
